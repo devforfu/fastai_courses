841dac3f14ff09590d0ea47e6fbc620eb8d7ad95
devforfu <developer.z@outlook.com>
2018-10-25 21:13:45 +0500

    Trying to deal with iMaterialist dataset

--- dl/lecture01/download_furniture.py
+++ dl/lecture01/download_furniture.py
@@ -1,5 +1,6 @@
 import os
 import json
+import shutil
 from pathlib import Path
 from dataclasses import dataclass, asdict
 from functools import partial
@@ -9,7 +10,6 @@ from multiprocessing import Pool, cpu_count
 import requests
 import numpy as np
 import pandas as pd
-from tqdm import tqdm
 from fastai.core import partition
 
 from logger import get_logger
@@ -33,21 +33,30 @@ def main():
         os.makedirs(dirname, exist_ok=True)
 
     paths = [
-        ('train', TRAIN_IMAGES),
-        ('valid', VALID_IMAGES),
-        ('test', TEST_IMAGES)]
+        ('train', TRAIN_IMAGES, 0.1),
+        ('validation', VALID_IMAGES, 0.2),
+        ('test', TEST_IMAGES, 0.5)]
 
-    for name, path in paths:
+    for name, path, fraction in paths:
+        sentinel = IMAGES / f'{name}.sentinel'
+
+        if sentinel.exists():
+            log.info('Already downloaded: %s', path)
+            continue
+
         json_file = PATH/f'{name}.json'
         index_file = PATH/f'{name}_index.csv'
 
         if not index_file.exists():
-            prepare_url_index(json_file, index_file)
+            prepare_url_index(json_file, index_file, pct=fraction)
 
         log.info(f'Downloading {json_file}...')
         index = pd.read_csv(index_file)
         info = download(index, path)
         info.to_pickle(IMAGES/f'{name}_info.pickle')
+
+        with sentinel.open('w') as file:
+            file.write('1')
 
     log.info('Done!')
 
@@ -63,14 +72,21 @@ def prepare_url_index(json_file, index_file, pct=0.1):
     """
     with json_file.open() as file:
         content = json.load(file)
-    images, labels = content['images'], content['annotations']
-    urls = [img['url'][0] for img in images]
 
+    images = content['images']
+    if 'annotations' in content:
+        labels = content['annotations']
+    else:
+        labels = [
+            {'image_id': img['image_id'], 'label_id': 0}
+            for img in images]
+
+    urls = [img['url'][0] for img in images]
     records = pd.DataFrame([
         {'url': url, **lbl}
         for url, lbl in zip(urls, labels)])
 
-    if pct is not None:
+    if pct is not None and pct < 1:
         pct = max(0.0, min(pct, 1.0))
         subsets = []
         for key, group in records.groupby('label_id'):
@@ -104,8 +120,21 @@ def download(index, path):
 
         for i, chunk in enumerate(chunks):
             log.info('Downloading chunk %d of %d' % (i+1, n_chunks))
-            data = [x for x in pool.imap_unordered(worker, chunk) if x]
-            meta.extend([asdict(info) for info in data])
+            running = True
+            count, total = 0, 3
+            while running:
+                try:
+                    data = [x for x in pool.imap_unordered(worker, chunk) if x]
+                    meta.extend([asdict(info) for info in data])
+                except:
+                    log.error('Something went wrong with pool downloader!')
+                    log.error('Trying again...')
+                    count += 1
+                    running = count >= total
+                    if not running:
+                        log.warning('Giving up to download chunk %d', i)
+                else:
+                    running = False
 
     return pd.DataFrame(meta)
 
@@ -121,7 +150,7 @@ def download_single(folder, info):
         return info
 
     try:
-        r = requests.get(url)
+        r = requests.get(url, timeout=60.0, stream=True)
         r.raise_for_status()
     except requests.HTTPError:
         log.warning(f'Cannot download URL: {url}')
@@ -129,11 +158,23 @@ def download_single(folder, info):
     except requests.ConnectionError:
         log.warning(f'Connection error! URL: {url}')
         return None
+    except requests.Timeout:
+        log.warning(f'Waiting response too long for URL: {url}')
+        return None
     except Exception:
         log.warning(f'Unexpected error while downloading URL: {url}')
         return None
+
+    # with path.open('wb') as file:
+    #     file.write(r.content)
+
     with path.open('wb') as file:
-        file.write(r.content)
+        r.raw.decode_content = True
+        shutil.copyfileobj(r.raw, file)
+
+    if os.stat(path).st_size <= 0:
+        log.warning('Empty image file content: %s', path)
+        return None
 
     return info
 
